6313c130fffd762387747d11dc88bff2800d732a
RohanYadav15 <rohanyadav.fb@gmail.com>
2024-10-14 23:48:30 -0500

    add login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,16 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/app_logo.png" alt="logo" aria-hiddne="true">
+    </header>
+    <section class="content">
+      <router-outlet></router-outlet>
+    </section>
+  </main>`,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'expense-tracker';
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  template: `
+  <section class="login-container">
+    <form [formGroup]="loginForm" (submit)="login()">
+      <input id="email" type="email" title="username" placeholder="username" />
+      <input id="password" type="password" placeholder="password"/>
+      <button class="submit" type="submit" [disabled]="!loginForm.valid">Login</button>
+    </form>
+  </section>`,
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+
+  login() {
+    let email = this.loginForm.value.email ?? '';
+    let password = this.loginForm.value.password ?? '';
+    console.log(email, password);
+  }
+}
